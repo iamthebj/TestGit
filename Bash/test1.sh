96f12be66f0549c40a065aae8dbe071b3ac95805
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-10 23:53:02 +0530

    Update test1.sh

--- Bash/test1.sh
+++ Bash/test1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-x=10            #NOT x = 10 no spaces
+x--2@10            #NOT x = 10 no spaces
 X=20            #variables are case sensitive
-$y=             #NULL variable
+$5             #NULL variable
 echo "x = $x"
 echo "X = $X"
 echo "y = $y"
