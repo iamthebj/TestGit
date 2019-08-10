af87d41139b0a4ebfbac6d36505c2cda23799437
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-10 22:51:16 +0530

    Update test1.kt

--- Kotlin/test1.kt
+++ Kotlin/test1.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val first = 1.5f
     val second = 2.0f
-    val product = first * second
-    println("The product is: $product")
+    val product = first second
+    printn(The product is: $product")
 }
