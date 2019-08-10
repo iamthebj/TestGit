c7e976f4ccfd29b45cf0d06ad67a299e8faa1974
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-10 22:34:30 +0530

    Update test2.rb

--- Ruby/test2.rb
+++ Ruby/test2.rb
@@ -1,7 +1,7 @@
 for i in (1..10)
     rno = rand(100) + 1
     msg = case rno
-        when 42: "The ultimate result."
+        when 42 "The ultimate result."
         when 1..10: "Way too small."
         when 11..15,19,27: "Sorry, too small"
         when 80..99: "Way to large"
@@ -9,6 +9,6 @@ for i in (1..10)
                 print "TOPS\n"
                 "Really way too large"
         else "Just wrong"
-    end
+    en
     print "Result: ", rno, ": ", msg, "\n"
 end
