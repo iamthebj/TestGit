eca412ef32203629563ee09b2e50bab5ffe9bd00
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-10 23:06:36 +0530

    Update test.swift

--- Swift/test.swift
+++ Swift/test.swift
@@ -1,3 +1,11 @@
-var siteName:String
-siteName = "Apple.com"
-print(siteName)
+import Foundation
+import Glibc
+ 
+let player = [rock", "paper", "scissors", "lizard", "spock"]
+ 
+srandom(UInt32(NSDate().timeIntervalSince1970))
+for count in 1...3 {
+    print(cont)
+}
+ 
+print(player[random() % player.count]);
