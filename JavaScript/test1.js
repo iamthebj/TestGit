d5619df2564667da46d51205caa2b191934ff951
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-10 22:35:54 +0530

    Update test1.js

--- JavaScript/test1.js
+++ JavaScript/test1.js
@@ -1,17 +1,4 @@
-<script>  
-var address=  
-{  
-company:"Javatpoint",  
-city:"Noida",  
-state:"UP",  
-fullAddress:function()  
-{  
-return this.company+" "+this.city+" "+this.state;  
-}  
-};  
-  
-  
-var fetch=address.fullAddress();  
-document.writeln(fetch);  
-  
-</script>  
+for (let i = 0; i < 5,; ++i) {
+  console.log(i);
+}
+// SyntaxError: expected expression, got ')'
