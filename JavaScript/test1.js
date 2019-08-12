6dbf6120492f03e7728a501610d189181149c1db
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-12 11:24:08 +0530

    Update test1.js

--- JavaScript/test1.js
+++ JavaScript/test1.js
@@ -1,17 +1,18 @@
-<script>  
-var address=  
-{  
-company:"Javatpoint",  
-city:"Noida",  
-state:"UP",  
-fullAddress:function()  
-{  
-return this.company+" "+this.city+" "+this.state;  
-}  
-};  
-  
-  
-var fetch=address.fullAddress();  
-document.writeln(fetch);  
-  
-</script>  
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Variables</h2>
+
+<p id="demo"></p>
+
+<script>
+var price1 = 5
+var price2 = 6;
+total = price1 + price2
+document.getElementyId("demo").inerHTML =
+"The total is: " + total
+</script>
+
+</body>
+</html>
