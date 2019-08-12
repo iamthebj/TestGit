2bd3e6ec80fe64eb1311499a722b04a2ed6684aa
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-12 11:14:28 +0530

    Update test1.js

--- JavaScript/test1.js
+++ JavaScript/test1.js
@@ -4,14 +4,11 @@ var address=
 company:"Javatpoint",  
 city:"Noida",  
 state:"UP",  
-fullAddress:function()  
-{  
+fullAddress:function(
 return this.company+" "+this.city+" "+this.state;  
-}  
-};  
   
   
 var fetch=address.fullAddress();  
-document.writeln(fetch);  
+document.writeln(fetch) 
   
 </script>  
