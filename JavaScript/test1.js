4c10c2e734ddc8008463b3dea603f54e354a2a52
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-12 11:20:07 +0530

    Update test1.js

--- JavaScript/test1.js
+++ JavaScript/test1.js
@@ -1,17 +1,9 @@
-<script>  
-var address=  
-{  
-company:"Javatpoint",  
-city:"Noida",  
-state:"UP",  
-fullAddress:function()  
-{  
-return this.company+" "+this.city+" "+this.state;  
-}  
-};  
-  
-  
-var fetch=address.fullAddress();  
-document.writeln(fetch);  
-  
-</script>  
+var obj = {};
+var obj.foo = 'hi'; // SyntaxError missing ; before statement
+
+var array = [];
+var array[0] = 'there'; // SyntaxError missing ; before statement
+
+
+
+
