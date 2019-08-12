d833e99ab13e62a514c0322c2950b91bbe7bf7cb
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-12 11:25:35 +0530

    Update test1.js

--- JavaScript/test1.js
+++ JavaScript/test1.js
@@ -1,17 +1,6 @@
-<script>  
-var address=  
-{  
-company:"Javatpoint",  
-city:"Noida",  
-state:"UP",  
-fullAddress:function()  
-{  
-return this.company+" "+this.city+" "+this.state;  
-}  
-};  
-  
-  
-var fetch=address.fullAddress();  
-document.writeln(fetch);  
-  
-</script>  
+<script>
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+document.getElementd("demo").innerHTML = "The total is: " + total;
+</script>
