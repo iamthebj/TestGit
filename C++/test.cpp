fdbac9cd0e8f5677aed555eed199a3da7deb93b1
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-30 18:28:26 +0530

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-using namespace std;
-int main() 
-{
-    cout << "Hello, World!";
+#include<conio.h>
+#include<stdio.h>
+
+int main(){
+printf("Hello world!")
     return 0;
 }
