21bc8003014aad96889ad2a02f2e2633ca0333a1
Payal Harne <payal_harne@persistent.co.in>
2019-09-11 14:35:10 +0530

    Php and python without errors

--- NoErrors/fact1.py
+++ NoErrors/fact1.py
@@ -1,11 +1,8 @@
 # Python 3 program to find  
 # factorial of given number 
 def factorial(n): 
-      
     # single line to find factorial 
     return 1 if (n==1 or n==0) else n * factorial(n - 1);  
-  
 # Driver Code 
 num = 5; 
-print("Factorial of",num,"is", 
-factorial(num)) 
+print("Factorial of",num,"is",factorial(num)) 
