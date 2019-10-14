1ce16617659e59f6a30fbf078976ed8fd09f3705
Payal <payalharne@yahoo.com>
2019-10-14 10:24:50 +0530

    error added

--- Java/FileDemo.java
+++ Java/FileDemo.java
@@ -4,7 +4,7 @@ public class FileDemo {
   
         try {  
             File file = new File("javaFile123.txt");  
-            if (file.createNewFile()) {  
+            (file.createNewFile()) {  
                 System.out.println("New File is created");
             } else {  
                 System.out.println("File already exists.");  
