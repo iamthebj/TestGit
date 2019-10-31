c944066141a6491c145f2d8dc612c44165aace13
nikoders5 <52191620+nikoders5@users.noreply.github.com>
2019-10-31 10:53:37 +0530

    Create Syn.py

---
+++ Syn.py
@@ -0,0 +1,8 @@
+    # print You Won
+    if failed == 0:        	    if failed == 0:        
+        print "	        print " You won"  
+You won"  	
+
+
+    # exit the script	    # exit the script
+        break              	        break              
