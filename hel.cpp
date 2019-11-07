cb4d71eda26d8764dfd887d75def8f57aef38428
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-11-07 11:42:37 +0530

    Create hel.cpp

---
+++ hel.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n = 1,times=5;                    /* local variable Initialization */  
+
+    for( n = 1; n <= times; n = n - 1 )     /* for loops execution */ 
+    {
+         cout << "C++ for loops: " << n <<endl;
+    }
+   return 0;
+}
